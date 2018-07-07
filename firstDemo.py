6b81e90f38f63366d3feb3a119a3f0e39db290e3
Hannah Scovill <hnsvill@gmail.com>
2018-07-06 18:03:27 -0700

    Adding notes file and Selenium example file

---
+++ firstDemo.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+elem = driver.find_element_by_name("q")
+elem.send_keys("Yamaha R6")
+elem.send_keys(Keys.RETURN)
+
+time.sleep(10)
+
+driver.quit()
+
