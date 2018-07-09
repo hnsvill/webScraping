194041744e36120acfc5c2877c0e3e5da0b2b897
Hannah Scovill <hnsvill@gmail.com>
2018-07-09 16:22:23 -0700

    testing reading a robots.txt tile

--- crawlerTest.py
+++ crawlerTest.py
@@ -1,3 +1,15 @@
-import bs4
+import urllib.robotparser
 
-print('import successful')
+webUrl = "http://www.musi-cal.com"
+rp = urllib.robotparser.RobotFileParser()
+
+rp.set_url(webUrl + "/robots.txt")
+rp.read()
+rrate = rp.request_rate("*")
+rrate.requests
+rrate.seconds
+rp.crawl_delay("*")
+
+rp.can_fetch("*", webUrl + "/cgi-bin/search?city=San+Francisco")
+
+rp.can_fetch("*", webUrl + "/")
